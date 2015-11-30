0652a3aec0563d93065089d545ffb8b52e66db13
Pkalra7 <pritish_kalra@yahoo.co.in>
2015-11-30 16:23:20 -0500

    singleton implementation added: only thing I'm commiting  since you're working on animalfactory.

--- animalCardFactory.cpp
+++ animalCardFactory.cpp
@@ -5,12 +5,32 @@
 #include "splitFour.h"
 #include "joker.h"
 
-AnimalFactory::AnimalFactory() {
+AnimalFactory::create()
+{
+	
+	if (count == 0)
+	{
+		obj = new Singleton();
+	}
 
+	return (obj);
+	
+}
+AnimalFactory::AnimalFactory() {  
+		
+	count = 1; 
+
+}
+
+AnimalFactory::~AnimalFactory()
+{
+	delete obj; obj = 0; count = 0;
 }
 
 AnimalFactory * AnimalFactory::getFactory() {
 	//not sure how to return the only instance of animalCardFactory, maybe static function?
+
+	return obj;
 }
 
 Deck<std::shared_ptr<AnimalCard>> AnimalFactory::getDeck() {
@@ -18,3 +38,5 @@ Deck<std::shared_ptr<AnimalCard>> AnimalFactory::getDeck() {
 	//admittedly don't know how we'll randomly generate 51 cards with no copies
 	return Deck<std::shared_ptr<AnimalCard>>();
 }
+
+
--- animalCardFactory.h
+++ animalCardFactory.h
@@ -2,8 +2,14 @@
 #include "deck.h"
 
 class AnimalFactory {
+	static int count;
+	static AnimalFactory *obj;
 public:
-	AnimalFactory();
+	static AnimalFactory* create()
 	static AnimalFactory* getFactory();
 	Deck<std::shared_ptr<AnimalCard>> getDeck();
+private:
+	AnimalFactory();
+	~AnimalFactory();
+	
 };
